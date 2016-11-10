c28aa88ae95d36c9b7ca581ae4a8c18493649f0a
Matt Davis <matt@techdavis.com>
2016-11-10 10:06:51 -0800

    Initial scan_multiple.  Working on iOS.  Added FocusLock button and Text Field overlay.

--- www/zbar.js
+++ www/zbar.js
@@ -14,9 +14,24 @@ ZBar.prototype = {
         if(params.text_instructions === undefined) params.text_instructions = "Please point your camera at the QR code.";
         if(params.camera != "front") params.camera = "back";
         if(params.flash != "on" && params.flash != "off") params.flash = "auto";
-
+        if(params.scan_multiple != true) params.scan_multiple = false;
+        if(params.play_beep != true) params.play_beep = false;
+        
         exec(success, failure, 'CsZBar', 'scan', [params]);
     },
+	stopScanning: function ()
+	{
+		exec(success, failure, 'CsZBar', 'stopScanning', null);
+	},
+	setDisplayText: function (textToDisplay,colorToDisplay,success, failure)
+	{
+		colorToDisplay= (typeof(colorToDisplay) !== 'undefined') ? colorToDisplay : '';
+		success= (typeof(success) !== 'undefined') ? colorToDisplay : function(winParam) {};
+		failure= (typeof(failure) !== 'undefined') ? failure : function(error) {};
+
+        params = {text: textToDisplay, color: colorToDisplay};
+		exec(success, failure, 'CsZBar', 'setDisplayText', [params]);
+	}
 
 };
 
